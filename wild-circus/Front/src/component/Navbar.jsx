7da8f658039737ead75d6596c54f143f5c13b60d
Victor Biard <vi.biard@gmail.com>
2020-01-29 16:26:19 +0100

    make some place in App.js and setup the new component navbar.js

---
+++ wild-circus/Front/src/component/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Navbar.css'
+
+function Navbar(props){
+    return(
+        <div className='navBarContainer'>
+            <ul>
+                <li>
+                    home
+                </li>
+                <li>
+                    About us
+                </li>
+                <li>
+                    News
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+            <img src="https://zupimages.net/up/20/05/8ol4.png" alt="logoFacebook"/>
+            <img src="https://zupimages.net/up/20/05/wcna.png" alt="logoTwitter"/>
+            <img src="https://zupimages.net/up/20/05/o5fl.png" alt="logoInstagram"/>
+        </div>
+      
+ 
+  
+    )
+};
+
+export default Sidebar;
