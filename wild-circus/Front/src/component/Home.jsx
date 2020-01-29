5da479e9ab1a9826ae8103e4bf21a205f5670907
Victor Biard <vi.biard@gmail.com>
2020-01-29 18:23:13 +0100

    pic and banner add

--- wild-circus/Front/src/component/Home.jsx
+++ wild-circus/Front/src/component/Home.jsx
@@ -4,7 +4,8 @@ import './Home.css'
 function Home() {
     return (
         <div className='HomeContainer'>
-            <p>Home component</p>
+            <img className='BannerCircus' src="https://zupimages.net/up/20/05/ee53.png" alt="bannerofcircus"/>
+            <img className='PicCircus' src="https://zupimages.net/up/20/05/duta.jpg" alt="Picofcircus"/>
         </div>
       
     );
--- wild-circus/Front/src/component/Navbar.jsx
+++ wild-circus/Front/src/component/Navbar.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import './Navbar.css'
+import './CssForAll.css'
 
 function Navbar(props){
     return(
-        <div className='NavBarContainer'>
-            <ul>
-                <li>
+        <div className='NavBarContainer Flex '>
+            <ul className='Flex SpaceAround MaxWidth ItemCenter'>
+                <li className='NoStyleList'>
                     home
                 </li>
-                <li>
+                <li className='NoStyleList'>
                     About us
                 </li>
-                <li>
+                <li className='NoStyleList'>
                     News
                 </li>
-                <li>
+                <li className='NoStyleList'>
                     Contact
                 </li>
             </ul>
-            <img src="https://zupimages.net/up/20/05/8ol4.png" alt="logoFacebook"/>
-            <img src="https://zupimages.net/up/20/05/wcna.png" alt="logoTwitter"/>
-            <img src="https://zupimages.net/up/20/05/o5fl.png" alt="logoInstagram"/>
+            <div className='logoNavContainer Flex SpaceAround ItemCenter'>
+                <img className='logoNavbar' src="https://zupimages.net/up/20/05/8ol4.png" alt="logoFacebook"/>
+                <img className='logoNavbar' src="https://zupimages.net/up/20/05/wcna.png" alt="logoTwitter"/>
+                <img className='logoNavbar' src="https://zupimages.net/up/20/05/o5fl.png" alt="logoInstagram"/>
+            </div>
         </div>
       
  
